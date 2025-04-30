88babe06ff02fb1472c949f3d891f9b0485b44d3
Rodrigo Slongo <rodrigo.slongotur@hotmail.com>
2025-04-30 16:30:07 -0300

    adiciona imagem

--- Portfolio/src/components/projects/Project.tsx
+++ Portfolio/src/components/projects/Project.tsx
@@ -3,7 +3,7 @@ import ProjectItem from "./ProjectItem";
 
 const Project = () => {
     const projects = [
-        {title: "AGES Study Group Repository", src: "./AGESPNB.png", href: "https://github.com/Slongo11/Grupo-de-estudos-AGES", alt: "Repository AGES", comment: "This repository documents the full journey within the AGES program, featuring 5 sprints and 5 distinct projects, each progressively more challenging and complex."},
+        {title: "AGES Study Group Repository", src: "./AGESPNB.jpeg", href: "https://github.com/Slongo11/Grupo-de-estudos-AGES", alt: "Repository AGES", comment: "This repository documents the full journey within the AGES program, featuring 5 sprints and 5 distinct projects, each progressively more challenging and complex."},
         {title: "Spring Framework Studies Repository", src: "./Spring.svg", href: "https://github.com/Slongo11/EstudosSpring", alt: "Repository Spring", comment: "This repository contains training projects from Alura focused on the Spring Framework."},
         {title: "Assembly Blackjack Game Repository", src: "./Assembly.png", href: "https://github.com/Slongo11/Trabalho-FSC", alt: "Repository Assembly", comment: "This repository features a Blackjack card game developed in Assembly language."},
     ]
